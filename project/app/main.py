a3b713b66830eb0491dfcb58dc34089a6ada8b22
Adam <adam@adamkielar.pl>
2020-10-30 20:24:35 +0100

    Postgres setup with tortoise

--- project/app/main.py
+++ project/app/main.py
@@ -1,8 +1,19 @@
+import os
+
 from fastapi import Depends, FastAPI
+from tortoise.contrib.fastapi import register_tortoise
 
 from .config import get_settings, Settings
 
 app = FastAPI()
+
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
 
 
 @app.get("/ping")
@@ -12,4 +23,3 @@ async def pong(settings: Settings = Depends(get_settings)):
         "environment": settings.environment,
         "testing": settings.testing
     }
-
