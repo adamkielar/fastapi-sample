816c2d6ed7d072e6eeaaa54fb677203dfbfed607
Adam Kielar <adam.kielar@netguru.com>
2020-11-08 17:02:10 +0100

    Added test and get summary by id endpoint

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,5 +1,14 @@
+from typing import Union
+
 from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
+
+
+async def get(id: int) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).first().values()
+    if summary:
+        return summary[0]
+    return None
 
 
 async def post(payload: SummaryPayloadSchema) -> int:
