c0f665ab4018e30b86d52d895d0e5f098d207b1f
Adam Kielar <adam.kielar@netguru.com>
2023-08-17 19:48:27 +0200

    Add script for AzDev

--- setup.sh
+++ setup.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 sleep 30
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "Installing Docker"
+# sudo apt-get install \
+#     apt-transport-https \
+#     ca-certificates \
+#     curl \
+#     gnupg-agent \
+#     software-properties-common
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+# sudo add-apt-repository \
+#    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#    $(lsb_release -cs) \
+#    stable"
+# sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-sudo usermod -aG docker AzDevOps
+# sudo usermod -aG docker AzDevOps
 
-sudo chmod 666 /var/run/docker.sock
+# sudo chmod 666 /var/run/docker.sock
